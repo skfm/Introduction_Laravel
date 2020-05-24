2fb8054e2561870eed17176c0d16f5668dfc2a99
SEIYA <skkasans@gmail.com>
2020-05-25 06:16:50 +0900

    フォルダ作成機能の追加
    ・フォルダ作成ページのルーティング追加
    ・フォルダ作成に関するコントローラー作成
    ・フォルダ作成ページのテンプレート作成
    ・フォルダ作成に関するバリデーションの追加

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -79,7 +79,7 @@ return [
     'max' => [
         'numeric' => ':attributeには、:max以下の数字を指定してください。',
         'file' => ':attributeには、:max kB以下のファイルを指定してください。',
-        'string' => ':attributeは、:max文字以下で指定してください。',
+        'string' => ':attributeは、:max文字以下で入力してください。',
         'array' => ':attributeは:max個以下指定してください。',
     ],
     'mimes' => ':attributeには:valuesタイプのファイルを指定してください。',
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
+
