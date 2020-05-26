4b419db673237f91949f99914b9390dee7049f60
SEIYA <skkasans@gmail.com>
2020-05-27 05:18:59 +0900

    認証機能の追加
    ・ユーザーテーブルとフォルダテーブルの紐づけ
    ・TOPページの追加
    ・会員登録機能の追加
    ・ログイン機能の追加
    ・バリエーションメッセージの修正

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
@@ -23,3 +26,7 @@ Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
